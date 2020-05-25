e3be68ed128bcd7fc76b42f56311f3c12bcd2809
relationToshima <toshima.keisuke@relation2012.com>
2020-05-25 18:13:16 +0900

    return project to bug

--- DBTest/src/main/java/com/example/demo/DbTestApplication.java
+++ DBTest/src/main/java/com/example/demo/DbTestApplication.java
@@ -1,13 +1,7 @@
 package com.example.demo;
 
-import javax.sql.DataSource;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class DbTestApplication {
@@ -15,7 +9,7 @@ public class DbTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DbTestApplication.class, args);
 	}
-
+/*
 	@Bean
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
 		final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
@@ -25,5 +19,5 @@ public class DbTestApplication {
 
 		return sessionFactory.getObject();
 	}
-
+*/
 }
--- DBTest/src/main/java/com/example/demo/domain/UserInfo.java
+++ DBTest/src/main/java/com/example/demo/domain/UserInfo.java
@@ -13,23 +13,23 @@ public class UserInfo{
 	/** 社員番号 */
 	    private String id;
 	    /** フリガナ */
-	    private String nameReading;
+	    private String namereading;
 	    /** 氏名 */
 	    private String name;
 	    /** メールアドレス */
-	    private String mailAddress;
+	    private String mailaddress;
 	    /** パスワード */
 	    private String password;
 	    /** 認証失敗回数 */
-	    private String failureTimes;
+	    private String failuretimes;
 	    /** 最終ログイン日 */
-	    private String lastLoginDate;
+	    private String lastlogindate;
 	    /** 登録者 */
 	    private String registrant;
 	    /** 登録日 */
-	    private String registrationDate;
+	    private String registrationdate;
 	    /** 更新者 */
 	    private String updater;
 	    /** 更新日 */
-	    private String updatedDate;
+	    private String updateddate;
 }
